8042394ca2a2fb4230131fa4e12ac948aa654ff4
Chason Choate <chason@bondware.com>
2013-09-15 23:55:14 -0500

    Added random scenario checking to see if a user ever wins, which probably won't happen. I'll let it run all night to see. o_O

--- js/index.js
+++ js/index.js
@@ -23,4 +23,37 @@ $(document).ready(function() {
         ].join());
         $('.modal').modal();
     });
+
+    // Additional testing. Running randomized games
+    // to see if there are any senarios where the computer
+    // loses.
+    $(document).keypress(function(e) {
+        if (e.which != 13) return;
+        var game = new TicTacToe();
+
+        $(document).off('game.gameover').on('game.gameover', function(e, obj) {
+            $('.box').html('');
+            if (obj.winner == 'human') {
+                console.log(game.board.toString());
+                console.log("Human won the scenario above.");
+                console.log('**************************************');
+            }
+            game = new TicTacToe();
+        });
+        var randomTest = function() {
+            // Find all empty boxes.
+            var emptyBoxes = [];
+            for (var i = 0; i < 3; i++) {
+               for (var j = 0; j < 3; j++) {
+                    if (game.board.get(i, j) == null)
+                        emptyBoxes.push([i, j]);
+               }
+            }
+            // randomly choose a position.
+            var pos = Math.floor(Math.random() * (emptyBoxes.length - 1));
+            game.humanMove(emptyBoxes[pos][0], emptyBoxes[pos][1]);
+            setTimeout(randomTest, 50);
+        };
+        randomTest();
+    });
 });
